9adf8c9b9c05db9fda262da565a0a2058d725a74
Abhinav Tushar <abhinav@lepisma.xyz>
2020-06-28 16:29:22 +0530

    add org-krita minor mode

--- org-krita.el
+++ org-krita.el
@@ -32,26 +32,74 @@
 (require 'archive-mode)
 (require 'filenotify)
 (require 'f)
+(require 'org)
 
-(defun org-krita-extract-png (kra-path output-path)
-  "Extract png from given KRA-PATH and save in OUTPUT-PATH"
-  (with-temp-file output-path
-    (archive-zip-extract kra-path "mergedimage.png")))
+(org-add-link-type "krita" #'org-krita-link-action #'org-krita-export)
 
-(defun org-krita-callback (event)
-  (let ((kra-path "./still.kra"))
-    (org-krita-extract-png kra-path (f-swap-ext kra-path "png"))
-    (with-current-buffer "test.org"
-      (org-redisplay-inline-images))))
+(defun org-krita-edit (png-path &optional full-mode)
+  "Edit given PNG-PATH in krita canvasonly mode.
 
-(defun org-krita-edit (png-path)
-  "Edit given png-path in krita."
+If FULL-MODE is not null, run full krita."
   (let ((kra-path (expand-file-name (f-swap-ext png-path "kra"))))
     (when (f-exists-p kra-path)
-      (call-process "krita" nil 0 nil "--canvasonly" "--nosplash" kra-path))))
+      (if full-mode
+          (call-process "krita" nil 0 nil kra-path)
+        (call-process "krita" nil 0 nil "--canvasonly" "--nosplash" kra-path)))))
 
-(file-notify-rm-watch "./still.kra")
-(file-notify-add-watch "./still.kra" '(change) 'org-krita-callback)
+(defun org-krita-link-action (path)
+  (org-krita-edit path))
+
+(defun org-krita-export (_path _desc _backend)
+  (error "Krita export not implemented yet."))
+
+(defun org-krita-extract-png (kra-path)
+  "Extract png from given KRA-PATH and return data."
+  (with-temp-buffer
+    (set-buffer-multibyte nil)
+    (archive-zip-extract kra-path "mergedimage.png")
+    (buffer-string)))
+
+;; TODO Add krita file watching
+;; (defun org-krita-callback (event)
+;;   (let ((kra-path "./still.kra"))
+;;     (org-krita-extract-png kra-path (f-swap-ext kra-path "png"))
+;;     (with-current-buffer "test.org"
+;;       (org-redisplay-inline-images))))
+
+;; (file-notify-rm-watch "./still.kra")
+;; (file-notify-add-watch "./still.kra" '(change) 'org-krita-callback)
+
+(defun org-krita-get-links ()
+  (org-element-map (org-element-parse-buffer) 'link
+    (lambda (link)
+      (when (string-equal (org-element-property :type link) "krita")
+        link))))
+
+(defun org-krita-show-link (link)
+  (let* ((start (org-element-property :begin link))
+         (end (org-element-property :end link))
+         (overlay (make-overlay start end))
+         (img (create-image (org-krita-extract-png (org-element-property :path link)) 'png t)))
+    (overlay-put overlay 'display img)))
+
+(defun org-krita-hide-all ()
+  (dolist (overlay (append (car (overlay-lists)) (cdr (overlay-lists))))
+    (delete-overlay overlay)))
+
+(defun org-krita-enable ()
+  (dolist (link (org-krita-get-links))
+    (org-krita-show-link link)))
+
+(defun org-krita-disable ()
+  (dolist (link (org-krita-get-links))
+    (file-notify-rm-watch (org-element-property :path link)))
+  (org-krita-hide-all))
+
+;;;###autoload
+(define-minor-mode org-krita-mode
+  "Mode for displaying editable krita images within Org Mode."
+  :initial-value nil
+  (if org-krita-mode (org-krita-enable) (org-krita-disable)))
 
 (provide 'org-krita)
 
