1cb843f66f0c6a96f863dede0fa959dac9d1fd9f
Abhinav Tushar <abhinav@lepisma.xyz>
2020-06-28 21:16:29 +0530

    auto-refresh

--- org-krita.el
+++ org-krita.el
@@ -32,22 +32,18 @@
 (require 'archive-mode)
 (require 'filenotify)
 (require 'f)
+(require 'cl-lib)
 (require 'org)
 
-(org-add-link-type "krita" #'org-krita-link-action #'org-krita-export)
+(org-add-link-type "krita" #'org-krita-edit #'org-krita-export)
 
-(defun org-krita-edit (png-path &optional full-mode)
-  "Edit given PNG-PATH in krita canvasonly mode.
+;; NOTE: Only single reference for a file supported as of now.
 
-If FULL-MODE is not null, run full krita."
-  (let ((kra-path (expand-file-name (f-swap-ext png-path "kra"))))
-    (when (f-exists-p kra-path)
-      (if full-mode
-          (call-process "krita" nil 0 nil kra-path)
-        (call-process "krita" nil 0 nil "--canvasonly" "--nosplash" kra-path)))))
+(defvar-local org-krita-watchers nil
+  "A-list mapping file names to change watcher descriptors.")
 
-(defun org-krita-link-action (path)
-  (org-krita-edit path))
+(defvar-local org-krita-overlays nil
+  "A-list mapping file names to overlay.")
 
 (defun org-krita-export (_path _desc _backend)
   (error "Krita export not implemented yet."))
@@ -59,45 +55,73 @@ If FULL-MODE is not null, run full krita."
     (archive-zip-extract kra-path "mergedimage.png")
     (buffer-string)))
 
-;; TODO Add krita file watching
-;; (defun org-krita-callback (event)
-;;   (let ((kra-path "./still.kra"))
-;;     (org-krita-extract-png kra-path (f-swap-ext kra-path "png"))
-;;     (with-current-buffer "test.org"
-;;       (org-redisplay-inline-images))))
-
-;; (file-notify-rm-watch "./still.kra")
-;; (file-notify-add-watch "./still.kra" '(change) 'org-krita-callback)
-
 (defun org-krita-get-links ()
   (org-element-map (org-element-parse-buffer) 'link
     (lambda (link)
       (when (string-equal (org-element-property :type link) "krita")
         link))))
 
+(defun org-krita-event-file-path (event)
+  (if (eq (nth 1 event) 'renamed)
+      (nth 3 event)
+    (nth 2 event)))
+
+(defun org-krita-watcher-callback (event)
+  "Callback that runs after krita files are modified."
+  (let* ((kra-path (org-krita-event-file-path event))
+         (links (org-krita-get-links))
+         (paths (mapcar (lambda (it) (expand-file-name (org-element-property :path it))) links))
+         (idx (cl-position kra-path paths :test #'string-equal)))
+    (when idx (org-krita-show-link (nth idx links)))))
+
+(defun org-krita-add-watcher (kra-path)
+  "Setup auto-refreshing watcher for given krita LINK."
+  (let ((desc (file-notify-add-watch kra-path '(change) #'org-krita-watcher-callback)))
+    (unless (alist-get kra-path org-krita-watchers nil nil #'string-equal)
+      (push (cons kra-path desc) org-krita-watchers))))
+
+(defun org-krita-edit (png-path &optional full-mode)
+  "Edit given PNG-PATH in krita canvasonly mode.
+
+If FULL-MODE is not null, run full krita."
+  (let ((kra-path (expand-file-name (f-swap-ext png-path "kra"))))
+    (when (f-exists-p kra-path)
+      (if full-mode
+          (call-process "krita" nil 0 nil kra-path)
+        (call-process "krita" nil 0 nil "--canvasonly" "--nosplash" kra-path))
+      (org-krita-add-watcher kra-path))))
+
+(defun org-krita-hide-link (link)
+  (let ((overlay (alist-get (org-element-property :path link) org-krita-overlays nil nil #'string-equal)))
+    (when overlay (delete-overlay overlay))))
+
 (defun org-krita-show-link (link)
+  (org-krita-hide-link link)
   (let* ((start (org-element-property :begin link))
          (end (org-element-property :end link))
          (overlay (make-overlay start end))
-         (img (create-image (org-krita-extract-png (org-element-property :path link)) 'png t)))
-    (overlay-put overlay 'display img)))
+         (kra-path (org-element-property :path link)))
+    (overlay-put overlay 'display (create-image (org-krita-extract-png kra-path) 'png t))
+    (push (cons kra-path overlay) org-krita-overlays)))
 
 (defun org-krita-hide-all ()
-  (dolist (overlay (append (car (overlay-lists)) (cdr (overlay-lists))))
-    (delete-overlay overlay)))
+  (dolist (link (org-krita-get-links))
+    (org-krita-hide-link link)))
 
 (defun org-krita-enable ()
   (dolist (link (org-krita-get-links))
     (org-krita-show-link link)))
 
 (defun org-krita-disable ()
-  (dolist (link (org-krita-get-links))
-    (file-notify-rm-watch (org-element-property :path link)))
+  "Disable watchers and hide krita images."
+  (dolist (watcher org-krita-watchers)
+    (file-notify-rm-watch (cdr watcher)))
+  (setq org-krita-watchers nil)
   (org-krita-hide-all))
 
 ;;;###autoload
 (define-minor-mode org-krita-mode
-  "Mode for displaying editable krita images within Org Mode."
+  "Mode for displaying editable krita images within Org file."
   :initial-value nil
   (if org-krita-mode (org-krita-enable) (org-krita-disable)))
 
